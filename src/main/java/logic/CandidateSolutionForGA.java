4157b4c22c86dca9f75808f55ef236ff07db793b
aaron <vorophobe@gmail.com>
2016-04-11 00:35:27 +0100

    small commit before bed: I rolled my own CandidateSolution class that allows me to access the CandidateAssignments list inside. I need to be able to randomise project preferences of candidates when generating an initial population, while still preserving the state of the candidate objects when they were first created (when the data was first injected into the class at runtime). is what I'm doing sane? hope so! also added TODOs to remind me to get the rest of Nil's suggestions implemented

--- src/main/java/logic/CandidateSolutionForGA.java
+++ src/main/java/logic/CandidateSolutionForGA.java
@@ -5,11 +5,11 @@ import model.Student;
 import java.util.Comparator;
 import java.util.List;
 
-class FitnessComparator implements Comparator<List<Student>> {
+class CandidateSolutionForGA implements Comparator<CandidateSolutionExtended> {
     // the fitness function atm is totally useless and doesn't return any
     // sensible fitness rating, but I just need the comparator to return
     // *some* value,
-    public int compare(List<Student> x, List<Student> y) {
+    public int compare(CandidateSolutionExtended x, CandidateSolutionExtended y) {
         if (fitness(x) < fitness(y)) {
             return -1;
         }
@@ -21,7 +21,7 @@ class FitnessComparator implements Comparator<List<Student>> {
 
     // TODO: make this not return sensible values
     // the comparator has it's own fitness function
-    private int fitness(List<Student> solution) {
+    private int fitness(CandidateSolutionExtended solution) {
         return (int) Math.floor(Math.random()) * 100;
     }
 }
--- src/main/java/logic/GeneticAlgorithm.java
+++ src/main/java/logic/GeneticAlgorithm.java
@@ -1,6 +1,5 @@
 package logic;
 
-import data.InMemoryRepo;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import model.DataSource;
@@ -11,9 +10,9 @@ import presentation.Result;
 import java.util.*;
 
 public class GeneticAlgorithm implements Solver {
-    private List<List<Student>> population;
+    private List<CandidateSolutionExtended> population;
     private Map<Integer,Project>  projects;
-    private InMemoryRepo repo;
+    private DataSource repo;
     private Map<Integer,Student> students;
     private Map<String,Student> studentRepo;
     private static int MAX_POPULATION = 10;
@@ -28,31 +27,50 @@ public class GeneticAlgorithm implements Solver {
     // create an initial population consisting of random solutions
     private void createInitialPopulation() {
         for (int i = 0; i < MAX_POPULATION; i++) {
-            List<Student> rand = createRandomSolution();
-            this.population.add(rand);
+            this.population.add(createRandomSolution());
         }
     }
 
-    private List<Student> createRandomSolution() {
-        List<Student> randomSolution = new ArrayList<>();
-        Set<Integer> studentKeySet = students.keySet();
+    // return a CandidateSolution containing candidates that have had their
+    // project preferences randomised
+    private CandidateSolutionExtended createRandomSolution() {
+        List<CandidateAssignment> randomisedCandidates = new ArrayList<>();
 
-        for (Integer k : studentKeySet) {
-            randomSolution.add(createRandomAssignment(students.get(k)));
+        // this object uses the same repo data that was used to inject data
+        // into this solver. The intention is to pull out a candidate from
+        // the repo, randomise their project preferences, and return them
+        CandidateSolutionExtended solution = new CandidateSolutionExtended(repo);
+
+        // iterate through every student that appears in the entrySet
+        for (Map.Entry<Integer, Student> pair : students.entrySet()) {
+            // get the candidate
+            CandidateAssignment cand = solution.getAssignmentFor(pair.getValue().getStudentName());
+            // randomise them
+            CandidateAssignment rand = randomiseAssignment(cand);
+            // add them to the list
+            randomisedCandidates.add(rand);
         }
-        return randomSolution;
+
+        //  update the solution with the list of randomised candidates
+        solution.setList(randomisedCandidates);
+
+        return solution;
     }
 
-    private Student createRandomAssignment(Student s) {
-        Project randomPref;
+    private CandidateAssignment randomiseAssignment(CandidateAssignment c) {
+        // clear the candidates current projects
+        c.getStudentEntry().getProjects().clear();
 
-        while (s.getNumberOfPreferences() < s.getNumberOfStatedPreferences()) {
-            randomPref = this.repo.getRandomPreference();
-            if (!s.hasPreference(randomPref)) {
-                s.addProject(randomPref);
+        // TODO: implement singleton random
+        // add some random projects, don't add duplicates!
+        while (c.getStudentEntry().getProjects().size() < c.getStudentEntry().getNumberOfStatedPreferences()) {
+            int r = (int) Math.floor(Math.random() * 100) % projects.size();
+
+            if (!c.getStudentEntry().hasPreference(projects.get(r))) {
+                c.getStudentEntry().addProject(projects.get(r));
             }
         }
-        return s;
+        return c;
     }
 
     // the strongest members of a population are more likely to survive, by
@@ -60,34 +78,33 @@ public class GeneticAlgorithm implements Solver {
     // members of a population have a tiny chance that they will survive.
     // conversely, a healthy specimen close to max fitness can sometimes die,
     // and less fit specimens can sometimes live and breed.
-    //
-    // calculating fitness (and culling) can be done with:
-    //   - a priority queue + comparator, then culling the lowest ordered (or
-    //   other variations on this theme)
-    //   - a specific fitness function that returns the relative fitness of
-    //   a student against the rest of the other objects in the set. if the
-    //   student is not fit enough, they are dropped from the collection
-    private List<List<Student>> cull(List<List<Student>> population) {
-        List<List<Student>> casualties = new ArrayList<>();
-        List<List<Student>> survivors = new ArrayList<>();
+    private List<CandidateSolutionExtended> cull(List<CandidateSolutionExtended> population) {
+        List<CandidateSolutionExtended> casualties = new ArrayList<>();
+        List<CandidateSolutionExtended> survivors = new ArrayList<>();
 
         int survivalChance = 0;
 
+        // TODO: implement Nil's suggestion below
+        // "All strategy can be squeezed into a single loop with parameter.
+        // Introduce an option, say, probability of mutation, and you random
+        // on the first line inside the loop, when your random is greater
+        // than that probability threshold, you don't pop from top, remove
+        // from random index etc...
+
         // create a student comparator to act as a temporary fitness function
-        Comparator<List<Student>> comparator = new FitnessComparator();
+        Comparator<CandidateSolutionExtended> comparator = new CandidateSolutionForGA();
 
         // create a priority queue to hold a collection of sorted solutions
-        PriorityQueue<List<Student>> orderedSolutions = new PriorityQueue<>(MAX_POPULATION, comparator);
+        PriorityQueue<CandidateSolutionExtended> orderedSolutions = new PriorityQueue<>(MAX_POPULATION, comparator);
 
         // add all solutions to the priority queue, in preparation for being ranked
-        for (List <Student> l : population) {
-                orderedSolutions.add(l);
+        for (CandidateSolutionExtended c : population) {
+            orderedSolutions.add(c);
         }
 
-        // strategy 0:
         // cull solutions based on their order in the priority queue
         for (int i = 0; i < MAX_POPULATION; i++) {
-            List<Student> curr = orderedSolutions.poll();
+            CandidateSolutionExtended curr = orderedSolutions.poll();
             survivalChance = (100 / MAX_POPULATION) * i;
 
             // cull the bottom 20% of student solutions
@@ -107,16 +124,6 @@ public class GeneticAlgorithm implements Solver {
         return survivors;
     }
 
-    // GA has it's own fitness function
-    private int fitness(List<Student> solution) {
-        return (int) Math.floor(Math.random()) * 100;
-
-        // fitness function given in Tony's slides
-        // fitness(TeamT) = ∑merit(PlayerT) - |∑cost(PlayerT) - 30|
-        //                  ---------------------------------------
-        //                              ∑cost(PlayerT)
-    }
-
     @Override
     public void InjectData(DataSource repo) {
         this.projects.clear();
@@ -129,8 +136,8 @@ public class GeneticAlgorithm implements Solver {
                 projects.put(idx++,p);
             }
 
-            this.repo = (InMemoryRepo) repo;
             studentRepo = repo.StudentRepo();
+            this.repo = repo;
 
             idx = 0;
             for (Student s : repo.StudentRepo().values()) {
@@ -153,16 +160,16 @@ public class GeneticAlgorithm implements Solver {
         }
 
         createInitialPopulation();
-        List<List<Student>> survivors = cull(this.population);
+        List<CandidateSolutionExtended> survivors = cull(this.population);
 
         // there is no presentation class for genetic algorithmss yet,
         // so I can't return a proper result
         // ... but I will triumph and create one!... after I do chores
-        System.out.println("====================================");
+        System.out.println("===================================");
         System.out.println("Battle report - Surviving solutions");
-        System.out.println("====================================");
-        for (List l: survivors) {
-            System.out.println(l.getClass());
+        System.out.println("===================================");
+        for (CandidateSolutionExtended c: survivors) {
+            System.out.println(c.toString()  + "\tFitness: " + c.getFitness());
         }
         System.out.println("==========================================");
         System.out.println("Battle report - Number of culled solutions");
