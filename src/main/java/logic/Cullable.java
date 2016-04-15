04c93fee00e48183ad906c3a17290d20e57c3e45
Ersi Ni <ersi.ni@nilbot.net>
2016-04-15 22:04:01 +0100

    Fix tests
    Since your fix took a while I thought I just lend you a
    hand, also because the tests were not working and the
    contagious throw statement in interface is source for
    other troubles.
    
    Change-Id: I6c96c6545dbfed0e96fcc25c872f21aa8bb1c663

--- src/main/java/logic/Cullable.java
+++ src/main/java/logic/Cullable.java
@@ -4,5 +4,5 @@ package logic;
  * Created by aaron on 15/04/16.
  */
 public interface Cullable {
-    GAData cull(CullConfig config, GAData input) throws Exception;
+    GAData cull(CullConfig config, GAData input);
 }
