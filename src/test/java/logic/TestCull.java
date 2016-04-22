589bb9678ff6baf87fc31b44e1d9550ea5438be5
aaron <vorophobe@gmail.com>
2016-04-22 12:27:11 +0100

    cull now uses Genepool and MockSolution. MockSolution has been extended for testing

--- src/test/java/logic/TestCull.java
+++ src/test/java/logic/TestCull.java
@@ -1,37 +1,47 @@
 package logic;
 
+import data.GenePool;
+import data.MockDataSource;
+import model.DataSource;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.internal.builders.NullBuilder;
 
 import static org.junit.Assert.*;
 
 public class TestCull {
-    CullConfig cullConfig;
-    GAData mock;
-    Cull cull;
+    private CullConfig cullConfig;
+    private Cull cull;
+    private GenePool<MockSolution_setAssignment> testPool;
+    private DataSource mockSource;
+    private MockSolution_setAssignment mockSol;
 
     @Before
     public void setup() {
-        cullConfig = new CullConfig();
-        mock = new GADataTestMock();
         cull = new Cull();
+        cullConfig = new CullConfig();
+        mockSource = new MockDataSource();
+        mockSol = new MockSolution_setAssignment(0);
+        testPool = new GenePool();
     }
 
     // logic tests
     @Test
-    public void TestCullMethod() throws Exception {
-        mock.initPopulation(100);
-        cullConfig.setCullAmount(10);
-        GAData result = cull.cull(cullConfig, mock);
-
-        assertEquals(90, result.getPopulationSize());
+    public void TestCull() throws Exception {
+        int n = 0;
+        for (int i = 0; i < 20; i++) {
+            mockSol = new MockSolution_setAssignment(n);
+            testPool.addToPool(mockSol);
+            n += 2;
+        }
+        cullConfig.setCullAmount(5);
+        testPool = cull.cull(cullConfig, testPool);
+        assertEquals(15, testPool.Size());
     }
 
     // smoke testing
     @Test(expected = IllegalArgumentException.class)
     public void TestCullSmoke() {
-        GAData dMock = null;
-        cull.cull(cullConfig, dMock);
+        GenePool mock = null;
+        cull.cull(cullConfig, mock);
     }
 }
