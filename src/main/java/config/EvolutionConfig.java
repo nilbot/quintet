50ab698fd4919877dc34cc478fbfaf3893aca880
Ersi Ni <ersi.ni@nilbot.net>
2016-04-28 23:12:59 +0100

    Minor refactor
    to warm up fingers
    
    Change-Id: Icf3035cfe2396f3d25be3aee28bfbb0ee095e407

--- src/main/java/config/EvolutionConfig.java
+++ src/main/java/config/EvolutionConfig.java
@@ -1,12 +1,26 @@
 package config;
 
+import logic.CombineConfig;
+import logic.Mutagen;
+import logic.Virus;
+
 /**
  * EvolutionConfig:
  */
 public class EvolutionConfig {
-    public int evolutionStepMax;
-
-    public void setStepMax(int stepMax) {
-        this.evolutionStepMax = stepMax;
+    public final CombineConfig combineConfig;
+    private final int killAmount = 5;
+    private final double probablityToKillGoodGuys = 0.2;
+    public final CullConfig cullConfig;
+    private int generationMax;
+    public EvolutionConfig() {
+        this.cullConfig = new CullConfig(killAmount, probablityToKillGoodGuys);
+        Mutagen mutagen = new Virus();
+        this.combineConfig = new CombineConfig(CombineConfig.NOBLE,
+                CombineConfig.MERGEBEST, mutagen);
     }
+    public void setStepMax(int stepMax) {
+        this.generationMax = stepMax;
+    }
+    public int generationMax() {return generationMax;}
 }
--- src/main/java/logic/GeneticAlgorithm.java
+++ src/main/java/logic/GeneticAlgorithm.java
@@ -9,23 +9,13 @@ import model.GeneticCandidateSolution;
  * GeneticAlgorithm:
  */
 public class GeneticAlgorithm {
-    private static final int max = 3000000;
-
     private final GenePool<GeneticCandidateSolution> genepool;
-    private final CullConfig cullConfig;
     private final EvolutionConfig config;
     private Cullable cullImplementor;
     private Combinable combineImplementor;
-    private final CombineConfig combineConfig;
-    private final int killAmount = 5;
-    private final double probablityToKillGoodGuys = 0.2;
 
     public GeneticAlgorithm(EvolutionConfig cfg) {
         this.genepool = new GenePool<>();
-        this.cullConfig = new CullConfig(killAmount, probablityToKillGoodGuys);
-        Mutagen mutagen = new Virus();
-        this.combineConfig = new CombineConfig(CombineConfig.NOBLE,
-                CombineConfig.MERGEBEST, mutagen);
         this.config = cfg;
         this.cullImplementor = new Cull();
         this.combineImplementor = new Combine();
@@ -34,9 +24,9 @@ public class GeneticAlgorithm {
 
     public GeneticCandidateSolution templateSolve() {
         populationInitialSeeding();
-        for (int i = 0; i < config.evolutionStepMax; i++){
-            cullImplementor.cull(this.cullConfig, this.genepool);
-            combineImplementor.combine(this.combineConfig,this.genepool);
+        for (int i = 0; i < config.generationMax(); i++){
+            cullImplementor.cull(config.cullConfig, this.genepool);
+            combineImplementor.combine(config.combineConfig,this.genepool);
         }
         return this.genepool.getBest();
     }
