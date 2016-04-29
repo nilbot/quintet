98f9f92c7c8c26424e1606f992d1b2640c906f99
Ersi Ni <ersi.ni@nilbot.net>
2016-04-29 22:12:57 +0100

    Add travis CI
    codeship's 100 free builds is about to used up.
    
    Change-Id: I0a82fe76abaa21f955ba8c56600e1b3e02e6bf53

--- src/main/java/presentation/AbstractResult.java
+++ src/main/java/presentation/AbstractResult.java
@@ -5,4 +5,5 @@ package presentation;
  */
 public abstract class AbstractResult extends GsonSerialzable {
     protected String solvingStrategy;
+    protected final String MessageType = "Result";
 }
