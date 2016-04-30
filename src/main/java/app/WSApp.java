f4970ab02a2eda506b3a64b3b5d3137772f57148
Ersi Ni <ersi.ni@nilbot.net>
2016-04-30 14:32:04 +0100

    [WIP] Websocket first working version (server echo)
    server is written in go
    
    Change-Id: Ie659b4071ae691c94638f59da35881257dd8edd1

--- src/main/java/app/WSApp.java
+++ src/main/java/app/WSApp.java
@@ -1,57 +1,57 @@
 package app;
 
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.drafts.Draft_17;
-import org.java_websocket.handshake.ServerHandshake;
 
+import org.glassfish.tyrus.client.ClientManager;
+
+import javax.websocket.*;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * WSApp:
  */
-public class WSApp extends WebSocketClient {
-    public WSApp(URI serverURI) {
-        super(serverURI,new Draft_17());
-    }
-
-    @Override
-    public void onOpen(ServerHandshake handshakeData) {
-        System.out.println("holyshit it connects?");
-    }
-
-    @Override
-    public void onMessage(String message) {
-        System.out.println("you gave me this:"+message);
-    }
-
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
-        System.out.println("i don't know how to handle close but here is what" +
-                " i got:");
-        System.out.println("remote?: "+remote+", code: "+code+", reason: " +
-                ""+reason);
-    }
-
-    @Override
-    public void onError(Exception ex) {
-        System.out.println("and I don't know how to handle error");
-    }
+public class WSApp {
+    private static CountDownLatch messageLatch;
+    private static final String SENT_MESSAGE = "Hello World";
 
     public static void main(String[] args) {
-        WSApp ws = null;
+        messageLatch = new CountDownLatch(1);
+        final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create
+                ().build();
+        ClientManager ws = ClientManager.createClient();
         try {
-            ws = new WSApp(new URI("ws://127" +
-                    ".0.0.1:8080/echo"));
+            ws.connectToServer(new Endpoint() {
+                @Override
+                public void onOpen(Session session, EndpointConfig config) {
+                    try {
+                        session.addMessageHandler(new MessageHandler.Whole<String>() {
+                            @Override
+                            public void onMessage(String message) {
+                                System.out.println("rx: " + message);
+                                messageLatch.countDown();
+                            }
+                        });
+                        session.getBasicRemote().sendText(SENT_MESSAGE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, cec, new URI("ws://localhost:8080/echo"));
+            messageLatch.await(100, TimeUnit.SECONDS);
+        } catch (DeploymentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         } catch (URISyntaxException e) {
             e.printStackTrace();
-        }
-        ws.connect();
-        try {
-            ws.send("Hello World".getBytes("UTF-8"));
-        } catch (UnsupportedEncodingException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
     }
 }
