a1843fcf316553eb85c42a980676f7372f1a54aa
Ersi Ni <ersi.ni@nilbot.net>
2016-05-01 13:47:43 +0100

    Attempt to fix exception
    Change-Id: I05afa876d768d2e8ac7120d5a8cf77d591c5cc6e

--- src/main/java/app/WSApp.java
+++ src/main/java/app/WSApp.java
@@ -47,76 +47,77 @@ public class WSApp {
 
         helloWorld();
 
+        sendMeta(meta);
+        sendResult(res);
+    }
+    private static void sendMeta(final InputMeta meta) {
+        messageLatch = new CountDownLatch(1);
         final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create
                 ().build();
-        ClientManager metaWS = new ClientManager().createClient();
-        Endpoint metaEndpoint = new Endpoint() {
-            @Override
-            public void onOpen(Session session, EndpointConfig config) {
-                try {
-                    session.addMessageHandler(new MessageHandler.Whole<String>
-                            () {
-                        @Override
-                        public void onMessage(String message) {
-                            throw new IllegalStateException(String.format("I " +
-                                    "should never receive anything: %v",
-                                    message));
-                        }
-                    });
-                    session.getBasicRemote().sendText(meta.toJson());
-                } catch (IllegalStateException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
+        ClientManager ws = ClientManager.createClient();
         try {
-            metaWS.connectToServer(metaEndpoint, cec, new URI("ws://127.0.0" +
-                    ".1:8080/meta"));
-        } catch (IOException e) {
-            e.printStackTrace();
+            ws.connectToServer(new Endpoint() {
+                @Override
+                public void onOpen(Session session, EndpointConfig config) {
+                    try {
+                        session.addMessageHandler(new MessageHandler.Whole<String>() {
+                            @Override
+                            public void onMessage(String message) {
+                                System.out.println("rx: " + message);
+                                messageLatch.countDown();
+                            }
+                        });
+                        session.getBasicRemote().sendText(meta.toJson());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, cec, new URI("ws://localhost:8080/meta"));
+            messageLatch.await(100, TimeUnit.SECONDS);
         } catch (DeploymentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         } catch (URISyntaxException e) {
             e.printStackTrace();
-        }
-        ClientManager resultWS = new ClientManager().createClient();
-        Endpoint resEndpoint = new Endpoint() {
-            @Override
-            public void onOpen(Session session, EndpointConfig config) {
-                try {
-                    session.addMessageHandler(new MessageHandler.Whole<String>
-                            () {
-                        @Override
-                        public void onMessage(String message) {
-                            throw new IllegalStateException(String.format("I " +
-                                            "should never receive anything: %v",
-                                    message));
-                        }
-                    });
-                    session.getBasicRemote().sendText(res.toJson());
-                } catch (IllegalStateException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        try {
-            resultWS.connectToServer(resEndpoint, cec, new URI("ws://127.0.0" +
-                    ".1:8080/meta"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (DeploymentException e) {
-            e.printStackTrace();
-        } catch (URISyntaxException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-
     }
-
+    private static void sendResult(final Result res) {
+        messageLatch = new CountDownLatch(1);
+        final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create
+                ().build();
+        ClientManager ws = ClientManager.createClient();
+        try {
+            ws.connectToServer(new Endpoint() {
+                @Override
+                public void onOpen(Session session, EndpointConfig config) {
+                    try {
+                        session.addMessageHandler(new MessageHandler.Whole<String>() {
+                            @Override
+                            public void onMessage(String message) {
+                                System.out.println("rx: " + message);
+                                messageLatch.countDown();
+                            }
+                        });
+                        session.getBasicRemote().sendText(res.toJson());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, cec, new URI("ws://localhost:8080/result"));
+            messageLatch.await(100, TimeUnit.SECONDS);
+        } catch (DeploymentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     private static void helloWorld() {
         messageLatch = new CountDownLatch(1);
         final ClientEndpointConfig cec = ClientEndpointConfig.Builder.create
