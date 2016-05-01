16bca250d0d2a84d97b3e940eb231fcf482837df
Ersi Ni <ersi.ni@nilbot.net>
2016-05-01 21:03:06 +0100

    Update auto deployment scripts
    Change-Id: Ib8c833d1cce5451f2459f20fab10bfa63ed31ae9

--- cronjob.sh
+++ cronjob.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 #Again, I know my environment; so whatev;
-kill -9 $(ps aux | grep '[b]ash cron' | awk '{print $2}')
+kill -9 $(ps aux | grep '[b]ash run' | awk '{print $2}')
 ./gradlew clean jar
-while true
-do
-	java -jar build/libs/quintet-1.2.0.jar
-	sleep 60
-done
+bash run.sh &>run.log &
+disown
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+while true
+do
+	java -jar build/libs/quintet-1.1.0.jar
+	sleep 60
+done
